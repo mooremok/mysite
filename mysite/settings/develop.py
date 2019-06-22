0f9af51b9a0b13caf31564408434f2ab7049820a
mooremok <mooremok@163.com>
2019-06-22 17:28:18 +0800

    hidden message

---
+++ mysite/settings/develop.py
@@ -0,0 +1,18 @@
+from .base import * #NOQA
+
+SECRET_KEY = '=99ny#f@9p)o(ntrv!89+8)*hs7!ig0of#t$j&gng^8^x#6k6@'
+
+DEBUG = True
+ALLOWED_HOSTS = []
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
+
+STATICFILES_DIRS = [
+   os.path.join(BASE_DIR, 'static')
+]
+
---
+++ mysite/settings/product.py
@@ -0,0 +1,56 @@
+from .base import * #NOQA
+
+SECRET_KEY = os.environ['SECRET_KEY']
+
+DEBUG = False
+ALLOWED_HOSTS = ['*']
+
+
+DATABASE_PASSWORD = os.environ['DATABASE_PASSWORD']
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'mysite_db',
+        'USER': 'root',
+        'PASSWORD': 'DATABASE_PASSWORD',
+        'HOST': 'localhost',
+        'PORT': '3306',
+    }
+}
+
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+ADMINS = (
+    ('admin', 'mooremok@163.com'),
+)
+
+# 日志文件
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'file': {
+            'level': 'DEBUG',
+            'class': 'logging.FileHandler',
+            'filename': '/home/mysite_debug.log',
+        },
+        'mail_admins': {
+            'level': 'ERROR',
+            'class': 'django.utils.log.AdminEmailHandler',
+        }
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['file'],
+            'level': 'DEBUG',
+            'propagate': True,
+        },
+        'django.request': {
+            'handlers': ['mail_admins'],
+            'level': 'ERROR',
+            'propagate': False,
+        },
+    },
+}
+
